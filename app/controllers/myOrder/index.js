250e40321a6d3043ecd92f8665ca0844548731bd
Nick930826 <xianyou1993@qq.com>
2016-06-03 18:14:32 +0800

    2016-6-3 我的订单页面基本完成 还差上评论图

--- app/controllers/myOrder/index.js
+++ app/controllers/myOrder/index.js
@@ -1,5 +1,38 @@
 'use strict';
 
-angular.module('app').controller('MyOrderIndexCtrl', function MyOrderIndexCtrl($scope) {
+angular.module('app').controller('MyOrderIndexCtrl', function MyOrderIndexCtrl($scope,ngDialog) {
   var vm = $scope.vm = {};
+  $scope.flag = false;
+  $scope.toggleOrder = function(){
+  	$scope.flag = !$scope.flag;
+  }
+  $scope.openUseExternalTemplate = function(){
+      ngDialog.open({
+        template: 'controllers/myOrder/popWindow-confirm.html',
+        plain: false,
+        className: 'ngdialog-theme-default',
+        closeByEscape: true,
+        closeByDocument: true
+      });
+    };
+
+	$scope.openUseExternalTemplate1 = function(){
+	  ngDialog.open({
+	    template: 'controllers/myOrder/popWindow-pay.html',
+	    plain: false,
+	    className: 'ngdialog-theme-default',
+	    closeByEscape: true,
+	    closeByDocument: true
+	  });
+	};
+
+	$scope.openUseExternalTemplate2 = function(){
+	  ngDialog.open({
+	    template: 'controllers/myOrder/popWindow-content.html',
+	    plain: false,
+	    className: 'ngdialog-theme-default',
+	    closeByEscape: true,
+	    closeByDocument: true
+	  });
+	};
 });
