feab69b7dda39313d895c556644e250e0ba51aca
Nick930826 <xianyou1993@qq.com>
2016-06-06 18:17:22 +0800

    2016-6-6 帐号管理

--- app/controllers/manage/index.js
+++ app/controllers/manage/index.js
@@ -1,5 +1,14 @@
 'use strict';
 
-angular.module('app').controller('ManageIndexCtrl', function ManageIndexCtrl($scope) {
+angular.module('app').controller('ManageIndexCtrl', function ManageIndexCtrl($scope,ngDialog) {
   var vm = $scope.vm = {};
+  $scope.openUseExternalTemplate = function(){
+	  ngDialog.open({
+	    template: 'controllers/manage/popCharge.html',
+	    plain: false,
+	    className: 'ngdialog-theme-default',
+	    closeByEscape: true,
+	    closeByDocument: true
+	  });
+	};
 });
