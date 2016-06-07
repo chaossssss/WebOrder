5168f9f0aa8261745ef8443823123a8cc14fc5c8
Nick930826 <xianyou1993@qq.com>
2016-06-07 18:55:42 +0800

    2016-6-7 完成消息记录 账号设置页头

--- app/controllers/setUp/index.js
+++ app/controllers/setUp/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
-angular.module('app').controller('SetUpIndexCtrl', function SetUpIndexCtrl($scope) {
+angular.module('app').controller('SetUpIndexCtrl', function SetUpIndexCtrl($scope,ngDialog) {
   var vm = $scope.vm = {};
+  $scope.toggle = true;
+  $scope.changeName = function(){
+  	$scope.toggle = !$scope.toggle;
+  }
+  $scope.openUseExternalTemplate = function(){
+      ngDialog.open({
+        template: 'controllers/setUp/popHeadPic.html',
+        plain: false,
+        className: 'ngdialog-theme-default',
+        closeByEscape: true,
+        closeByDocument: true
+      });
+    };
 });
