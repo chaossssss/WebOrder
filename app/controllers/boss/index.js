f3e14ff31c06b53922ecde0611af57eadad407b8
Nick930826 <xianyou1993@qq.com>
2016-06-11 19:26:55 +0800

    商户

--- app/controllers/boss/index.js
+++ app/controllers/boss/index.js
@@ -2,5 +2,22 @@
 
 angular.module('app').controller('BossIndexCtrl', function BossIndexCtrl($scope,ngDialog) {
 	var vm = $scope.vm = {};
-
+	$scope.infoshow = true;
+    $scope.infoshow1 = false;
+    $scope.infoshow2 = false;
+    $scope.toggle = function(){
+    	$scope.infoshow = true;
+    	$scope.infoshow1 = false;
+    	$scope.infoshow2 = false;
+    }
+    $scope.toggle1 = function(){
+    	$scope.infoshow = false;
+    	$scope.infoshow1 = true;
+    	$scope.infoshow2 = false;
+  	}
+  	$scope.toggle2 = function(){
+    	$scope.infoshow = false;
+    	$scope.infoshow1 = false;
+    	$scope.infoshow2 = true;
+  	}
 })	
