2b61c249d6b467f8465f78a3d708012fec1c68b2
Nick930826 <xianyou1993@qq.com>
2016-06-11 18:12:16 +0800

    工人

--- app/controllers/worker/index.js
+++ app/controllers/worker/index.js
@@ -2,4 +2,14 @@
 
 angular.module('app').controller('WorkerIndexCtrl', function WorkerIndexCtrl($scope) {
   var vm = $scope.vm = {};
+  $scope.infoshow = true;
+  $scope.infoshow1 = false;
+  $scope.toggle = function(){
+  	$scope.infoshow = true;
+  	$scope.infoshow1 = false;
+  }
+  $scope.toggle1 = function(){
+  	$scope.infoshow = false;
+  	$scope.infoshow1 = true;
+  }
 });
